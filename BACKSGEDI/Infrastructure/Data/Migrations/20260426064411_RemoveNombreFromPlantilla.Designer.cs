29c2def92a2bcb9a050251773566d4e87e35bb3d
Jadps <jadp.xs@gmail.com>
2026-04-26 00:49:05 -0600

    fix: Missing plantillas table migration

--- BACKSGEDI/Infrastructure/Data/Migrations/20260426064411_RemoveNombreFromPlantilla.Designer.cs
+++ BACKSGEDI/Infrastructure/Data/Migrations/20260426064411_RemoveNombreFromPlantilla.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BACKSGEDI.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BACKSGEDI.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260426064411_RemoveNombreFromPlantilla")]
+    partial class RemoveNombreFromPlantilla
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BACKSGEDI/Infrastructure/Data/Migrations/20260426064411_RemoveNombreFromPlantilla.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BACKSGEDI.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNombreFromPlantilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "PlantillasDocumentos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "PlantillasDocumentos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
