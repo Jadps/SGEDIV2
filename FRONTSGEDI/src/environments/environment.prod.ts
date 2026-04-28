407217568517c18a1c11fd16f9da72182a6e785b
Jadps <jadp.xs@gmail.com>
2026-04-27 18:54:49 -0600

    wip: Deploy

--- FRONTSGEDI/src/environments/environment.prod.ts
+++ FRONTSGEDI/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.sgediv2.com/api',
-  hubUrl: 'https://api.sgediv2.com/hubs'
+  apiUrl: 'https://sgedi.api.alonsodev.online/api',
 };
--- FRONTSGEDI/src/environments/environment.ts
+++ FRONTSGEDI/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:5095/api',
-  hubUrl: 'http://localhost:5095/hubs'
 };
